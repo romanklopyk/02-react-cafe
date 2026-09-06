379ba3fe7ab75f913532cb9215457fc92d309cff
romankhlopyk <romankhlopyk@outlook.com>
2026-09-06 08:50:18 +0300

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './components/App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.querySelector('#root');
+
+if (!container) {
+  throw new Error('Root container not found');
+}
+
+const root = createRoot(container);
+
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
