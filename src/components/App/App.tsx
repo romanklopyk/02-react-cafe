5ac8ab6ee93cbd1c9a5dbd631fea05e390f01c69
romankhlopyk <romankhlopyk@outlook.com>
2026-09-06 12:26:00 +0300

    add folder for types-  votes.ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import CafeInfo from '../CafeInfo/CafeInfo.tsx';
 import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 import VoteStats from "../VoteStats/VoteStats.tsx";
 import Notification from "../Notification/Notification.tsx";
-import type {VoteType, Vote} from "../../types/types.ts";
+import type {VoteType, Vote} from "../../types/votes.ts";
 
 function App() {
 
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,5 +1,5 @@
 import css from "./VoteOptions.module.css";
-import type {VoteType} from "../../types/types.ts";
+import type {VoteType} from "../../types/votes.ts";
 
 interface VoteOptionsProps {
     handleVote: (vote: VoteType) => void;
